d0623d9f3bd36e15962a777d6044efbfd5feed93
auraofdawn <aura.of.dawn.2013@gmail.com>
2023-01-01 02:51:35 -0500

    Revert "Display bootstrapping progress logs every 30s (#2131)"
    This reverts commit e385ec8e29b2f1a0652854d0f9ccfb73c560423e.

--- snow/engine/common/queue/jobs.go
+++ snow/engine/common/queue/jobs.go
@@ -20,7 +20,11 @@ import (
 	"github.com/ava-labs/avalanchego/utils/wrappers"
 )
 
-const progressUpdateFrequency = 30 * time.Second
+const (
+	// StatusUpdateFrequency is how many containers should be processed between
+	// logs
+	StatusUpdateFrequency = 2500
+)
 
 // Jobs tracks a series of jobs that form a DAG of dependencies.
 type Jobs struct {
@@ -108,7 +112,6 @@ func (j *Jobs) ExecuteAll(ctx *snow.ConsensusContext, halter common.Haltable, re
 	numExecuted := 0
 	numToExecute := j.state.numJobs
 	startTime := time.Now()
-	lastProgressUpdate := startTime
 
 	// Disable and clear state caches to prevent us from attempting to execute
 	// a vertex that was previously parsed, but not saved to the VM. Some VMs
@@ -176,7 +179,7 @@ func (j *Jobs) ExecuteAll(ctx *snow.ConsensusContext, halter common.Haltable, re
 		}
 
 		numExecuted++
-		if time.Since(lastProgressUpdate) > progressUpdateFrequency { // Periodically print progress
+		if numExecuted%StatusUpdateFrequency == 0 { // Periodically print progress
 			eta := timer.EstimateETA(
 				startTime,
 				uint64(numExecuted),
@@ -197,8 +200,6 @@ func (j *Jobs) ExecuteAll(ctx *snow.ConsensusContext, halter common.Haltable, re
 					zap.Duration("eta", eta),
 				)
 			}
-
-			lastProgressUpdate = time.Now()
 		}
 	}
 
